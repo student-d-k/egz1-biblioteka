f3ea2e79fd4e700a4f8d130bad2eef9fe64a6304
student-d-k <daumantas.kudzma@codeacademylt.onmicrosoft.com>
2024-10-14 15:20:49 +0300

    library funkcionalumai sukelti is main.py i library.py

--- egz1_biblioteka/classes/library.py
+++ egz1_biblioteka/classes/library.py
@@ -8,133 +8,125 @@ from classes.booking_record import *
 
 BOOK_BORROW_MAX_DAYS = 7 # kiek laiko galima laikyti knyga
 
+
 class Library:
 
     def __init__(self) -> None:
         self.users = {}
         self.books = {}
-        # self.authors = {}
         self.booking_records = []
         self.genres = [Poetry(), Epic(), Fiction(), Story(), Children(), Detective()]
 
 
 # 1 prideti knyga i biblioteka
 
-def add_book_to_library(library: Library, role: Role, book: Book):
-    """
-    Adds books to library.
-
-    Parameters:
-    library (Library): The library where to add the book.
-    role (Role): The role of the person attempting to add the book.
-    book (Book): The book to be added.
-    """
+def add_book_to_library(library: Library, book: Book) -> str:
+    'return error or empty string'
 
     try:
 
-        if role.can_add_book():
-
-            if book in library.books:
-                library.books[book][0] += 1
-            else:
-                library.books[book] = 1
-
-            print(f'Pridėjau knygą: "{book}".')
-
+        if book in library.books:
+            library.books[book][0] += 1
         else:
-            print('Jūs neturite teisių pridėti knygos.')
+            library.books[book] = 1
+
+        return ''
 
     except Exception as e:
 
-        print(e)
+        return str(e)
 
-# 2 pasalinti senas knygas is bibliotekos
 
-def delete_book_from_library(library: Library, role: Role, filter_function: callable):
-    """
-    Deletes books from the library based on a filter function.
+# 2 pasalinti knyga is bibliotekos
 
-    Parameters:
-    library (Library): The library from which to delete books.
-    role (Role): The role of the person attempting to delete the book.
-    filter (callable): A function that takes a book and returns True if the book should be deleted.
-    """
+def delete_book_from_library(library: Library, book: Book) -> str:
+    'return error or empty string'
 
     try:
 
-        if not role.can_delete_book():
-            print('Jūs neturite teisių ištrinti knygos.')
-            return
+        library.books.pop(book)
 
-        delete_list_books = [e for e in filter(filter_function, library.books)]
+        delete_list_booking_records = [(i, br) for i, br in enumerate(library.booking_records) if br.book == book]
+        delete_list_booking_records.sort(reverse=True, key=lambda x: x[0])
 
-        for book in delete_list_books:
+        # istrinam susijusius irasus ir booking_records
 
-            library.books.pop(book)
-            print(f'Ištryniau knygą "{str(book)}"')
+        for i, br in delete_list_booking_records:
+            r = library.booking_records.pop(i)
 
-            delete_list_booking_records = [(i, br) for i, br in enumerate(library.booking_records) if br.book == book]
-            delete_list_booking_records.sort(reverse=True, key=lambda x: x[0])
-
-            for i, br in delete_list_booking_records:
-                r = library.booking_records.pop(i)
-                print(f'Ištryniau įrašą {str(r)}')
+        return ''
 
     except Exception as e:
 
-        print(e)
+        return str(e)
 
-# 3, 8 pasiimti knyga issinesimui
 
-def borrow_book_from_library(library: Library, role: Role, user_id: User, book: Book):
-    """
-    Borrows book from the library based on a customer.
+# help function
 
-    Parameters:
-    library (Library): The library from which to borrow book.
-    role (Role): The role of the person attempting to borrow the book.
-    user (User): The user attempting to borrow the book.
-    book (Book): The book to be borrowed.
-    """
+def available_books(library: Library, book: Book) -> int:
+    'kiek knygos egzemplioriu liko bibliotekoje'
+
+    booked_books = [e.book for e in filter(lambda br: br.book == book, library.booking_records)]
+    return library.books[book] - len(booked_books)
+
+
+# 3 pasiimti knyga issinesimui
+
+def borrow_book_from_library(library: Library, user_id: User, book: Book):
+    'return error or empty string'
 
     try:
-
-        if not role.can_borrow_book(): 
-            print('Jūs neturite teisių išsinuomoti knygos.')
-            return
 
         if not book in library.books:
-            print('Bibliotekoje nėra tokios knygos.')
-            return
-
-        if library.books[book] == 0:
-            print('Visos knygos išnuomotos.')
-            return
-
-        delayed_books = [e.book for e in \
-                        filter(lambda br: br.user_id == user_id and \
-                                          (datetime.datetime.now()-br.created_on).days > BOOK_BORROW_MAX_DAYS, \
-                        library.booking_records)]
-
-        if len(delayed_books) > 0:
-            print('Jūs turite laiku negrąžintų knygų.')
-            return
+            return 'No such book in library'
 
         already_have_booking = [e.book for e in filter(lambda br: br.user_id == user_id and br.book == book, library.booking_records)]
 
         if len(already_have_booking) > 0:
-            print('Jūs jau esate pasiėmęs šitą knygą.')
-            return
+            return 'You have already booked this book'
+
+        if available_books(library, book) < 1:
+            return print('All books booked')
 
         # pridedam irasa
 
         library.booking_records.append(BookingRecord(len(library.booking_records)+1, Borrow(), user_id, book))
 
-        print(library.booking_records[-1])
+        return ''
 
     except Exception as e:
+        return str(e)
 
-        print(e)
 
 # 4, 5, 6, 7 ieskoti knygu pagal pavadinima arba autoriu arba pagal velavima
 
+def get_books_by_filter(library: Library, search_str: str, year_from: int, year_to: int, only_overdued: bool, only_available: bool) -> [Book]:
+
+        if search_str == '*':
+            filter_books_0 = [book for book in library.books if year_from <= book.year_of_release <= year_to]
+
+        else:        
+            filter_books_0 = [
+                book for book in library.books
+                if year_from <= book.year_of_release <= year_to
+                and (search_str.lower() in book.author.author_name.lower() or search_str in book.caption.lower())
+            ]
+
+        if only_overdued:
+            overdued_books = [e.book for e in filter(lambda br: (datetime.datetime.now()-br.created_on).days > BOOK_BORROW_MAX_DAYS, library.booking_records)]
+
+        if only_available:
+            available_books = [book for book in library.books if available_books(library, book) > 0]
+
+        if only_overdued:
+            if only_available:
+                filter_books = [item for item in filter_books_0 if item in overdued_books and item in available_books]
+            else:
+                filter_books = [item for item in filter_books_0 if item in overdued_books]
+        else:
+            if only_available:
+                filter_books = [item for item in filter_books_0 if item in available_books]
+            else:
+                filter_books = filter_books_0
+
+        return filter_books
